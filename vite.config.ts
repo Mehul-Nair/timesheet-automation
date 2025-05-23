01866f566b4ed88708df3ff2e3201a73d5260523
Mehul Nair <mehul.nair@teknopoint.in>
2025-05-23 15:41:25 +0530

    added frontend url

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
+  // define: {
+  //   "import.meta.env.BACKEND_URL": JSON.stringify(process.env.BACKEND_URL),
+  // },
 });
